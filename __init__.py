91b5d7f4bc643c4cd49da2804e7c4be1e02547ea
Santiago Yeomans <santiagoyeomansmolina@gmail.com>
2019-07-23 23:10:47 -0700

    .

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from gym.envs.registration import register
+
+register(
+    id='test-v0',
+    entry_point='gym_test.envs:TestEnv',
+)
+register(
+    id='test-extrahard-v0',
+    entry_point='gym_test.envs:TestExtraHardEnv',
+)
---
+++ envs/__init__.py
@@ -0,0 +1,2 @@
+from gym_test.envs.test_env import TestEnv
+from gym_test.envs.test_extrahard_env import TestExtraHardEnv
